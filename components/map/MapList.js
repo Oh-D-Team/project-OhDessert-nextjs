acec7902b0efaf20767e9acc680518a5d3acd01b
yoonhee Lee <105180536+yoonhee032@users.noreply.github.com>
2023-02-23 15:28:21 +0900

    MapList input창 추가

--- components/map/MapList.js
+++ components/map/MapList.js
@@ -1,18 +1,62 @@
 import React from "react";
+import { useEffect, useState } from "react";
 
-export default function MapList(){
-    return(
+import MapItem from "./MapItem";
+import Detail from "./Detail";
+
+import Search from "../../public/search.svg";
+import SolidArrow from "../../public/solid_arrow.svg";
+
+const MapList = (props) => {
+  const [shopData, setShopData] = useState([]);
+
+  useEffect(() => {
+    setShopData(props.data.products);
+  });
+
+  useEffect(() => {
+    console.log("리스트의 props입니다.", props.data.products);
+  }, []);
+
+  const [showDetail, setShowDetail] = useState(false);
+
+  return (
     <>
-        <section className="bg-white w-full h-[46%] bottom-0 sm:w-[400px] sm:h-[calc(100vh-80px)] fixed left-0 z-[1] drop-shadow-lg overflow-y-auto">
-            <div className="px-5 py-5 flex gap-5 items-center border-b border-[#ededed] hover:bg-[#fafafa]">
-                <div className="w-[140px] h-[100px] rounded-lg overflow-hidden"><img src="/img/map_list_image.jpg" className="object-cover w-full h-full" /></div>
-                <div className="text-black w-[180px]">
-                    <h3 className="text-lg font-bold truncate">딩가케이크 연남점</h3>
-                    <p className="text-sm mt-2"><strong className="mr-2">영업중</strong>11:30 ~ 22:00</p>
-                    <p className="text-sm text-[#999] mt-1 truncate">서울 마포구 동교로 29길 68</p>
-                </div>
-            </div>
-        </section>
+      <section className="pt-10 bg-white w-full h-[46%] bottom-0 sm:w-[400px] sm:h-[calc(100vh-70px)] fixed left-0 z-[1] drop-shadow-lg overflow-y-auto">
+        <div className="p-5 border-b-8 border-[#f5f5f5]">
+          <form className="relative">
+            <input
+              type="search"
+              placeholder="가게 이름, 지역, 키워드 검색"
+              className="border-2 border-[#F53354] w-full h-12 rounded-lg pl-12 placeholder:text-[#bbb]"
+            />
+            <button
+              type="submit"
+              className="absolute top-1/2 left-4 translate-y-[-50%]"
+            >
+              <Search />
+            </button>
+          </form>
+          <div className="flex mt-5 gap-2">
+            <button className="h-[36px] bg-[#ededed] flex items-center px-3 rounded text-[#666]">
+              <p className="pr-2">디자인 컨셉</p>
+              <SolidArrow />
+            </button>
+            <button className="h-[36px] bg-[#ededed] flex items-center px-3 rounded text-[#666]">
+              <p className="pr-2">상황</p>
+              <SolidArrow />
+            </button>
+          </div>
+        </div>
+
+        {shopData.map((data, index) => (
+          <div key={`${data.title}-${data.latitude}`}>
+            <MapItem data={data} />
+          </div>
+        ))}
+      </section>
     </>
-    )
-}
+  );
+};
+
+export default MapList;
