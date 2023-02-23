39833d0082d853f604d745c45286b569b04e83ce
yoonhee Lee <105180536+yoonhee032@users.noreply.github.com>
2023-02-23 15:24:35 +0900

    마커 css 수정

--- components/map/Overlay.js
+++ components/map/Overlay.js
@@ -4,10 +4,14 @@ const Overlay = (props) => {
   return (
     <div className="wrap">
       <div className="info">
-        
-        <div className="px-3 py-4 bg-[#f1f1f1]">{props.data.title}</div>
+        <div
+          className="px-4 py-3 shadow-lg text-sm bg-[#F53354] relative font-medium text-center
+         text-white rounded-full after:'' after:w-0 after:h-0 after:border-8 after:border-[#F53354] 
+         after:absolute after:-bottom-4 after:left-1/2 after:translate-x-[-50%] after:border-r-transparent after:border-b-transparent after:border-l-transparent"
+        >
+          {props.data.title}
+        </div>
         <div className="body">
-          
           <div className="desc">
             {/* <div className="ellipsis">{props.data.address}</div> */}
             {/* <div className="jibun ellipsis">(우) 63309 (지번) 영평동 2181</div> */}
