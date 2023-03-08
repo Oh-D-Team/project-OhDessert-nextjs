75813fc7f9f2ea67557f943bcd5fd61b153e0c80
yoonhee Lee <105180536+yoonhee032@users.noreply.github.com>
2023-03-08 12:01:47 +0900

    큐레이션 css 추가

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,20 @@
+import Script from "next/script";
+
 import Layout from "../components/Layout";
+
 import "../styles/globals.css";
 import "../styles/index.css";
+import "../styles/curation.css";
+
+const KAKAO_SDK_URL = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAOMAP_APPKEY}&autoload=false`;
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Script src={KAKAO_SDK_URL} strategy="beforeInteractive" />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
